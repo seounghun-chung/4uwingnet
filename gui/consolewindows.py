8b9487e443b52a7b335976601c7ce6d42ac13a5f
seounghun.chung <4uwingnet@naver.com>
2018-05-20 21:05:43 +0900

    [pytool] console input was changed from lineedit to combobox

--- gui/consolewindows.py
+++ gui/consolewindows.py
@@ -1,10 +1,9 @@
 from feature import consolemanager
-
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui, QtCore
 from PyQt5 import uic
-from PyQt5.QtCore import QObject
-from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import QObject, pyqtSignal, Qt
+
 import os
 import sys
 import threading
@@ -44,37 +43,37 @@ class ConsoleWindows(QWidget, form_class):
     def __init__(self, parent = None):
         super(ConsoleWindows, self).__init__(parent)
         self.setupUi(self)
-
+        
         self._stdout = StdoutRedirect()
         self._stdout.printOccur.connect(lambda x, y: self.append_text(x, y)) # print redirection
 
-        self.lineEdit.returnPressed.connect(self.lineEdit_returnPressed)
-        self.lineEdit.dragEnterEvent = self._lineEdit_dragEnterEvent
-        self.lineEdit.dropEvent = self._lineEdit_dropEvent
-
+        self.comboBox.keyPressEvent = self.comboBox_keyPressEvent
+        
     def stdout_redirect(self, s):
         if s is True:
             self._stdout.start()
         else:
             self._stdout.stop()
-
-    def lineEdit_returnPressed(self):
-        self.textBrowser.moveCursor(QtGui.QTextCursor.End)
-        cmd = self.lineEdit.displayText()
-        self.lineEdit.clear()
-
-        # next command list is not controlled by consolemanager.py
-        # so then, directly handle these in here
-        if (cmd == ""):
-            pass
-        elif (cmd == "help()"): # help() occurs error in GUI console widget
-            consolemanager.cexec("info()")
-        elif (cmd == "exit()"): # exit() occurs error in exec module
-            exit(0)
+        
+    def comboBox_keyPressEvent(self, e):
+        QComboBox.keyPressEvent(self.comboBox, e)
+        if e.key() == Qt.Key_Return:
+            cmd = self.comboBox.currentText()
+            self.textBrowser.moveCursor(QtGui.QTextCursor.End)
+            self.comboBox.setCurrentText("")
+            
+            if (cmd == ""):
+                pass
+            elif (cmd == "help()"): # help() occurs error in GUI console widget
+                consolemanager.cexec("info()")
+            elif (cmd == "exit()"): # exit() occurs error in exec module
+                exit(0)
+            else:
+                self.append_text(">>> " + cmd + "\n")
+                consolemanager.cexec(cmd)        
         else:
-            self.append_text(">>> " + cmd + "\n")
-            consolemanager.cexec(cmd)
-
+            pass
+            
     def append_text(self, msg, color="black"):
         self.textBrowser.moveCursor(QtGui.QTextCursor.End)
         self.textBrowser.setTextColor(QtGui.QColor(color))
@@ -83,12 +82,6 @@ class ConsoleWindows(QWidget, form_class):
 
     def clear(self):
         self.textBrowser.clear()
+        self.comboBox.clear()
 
-    def _lineEdit_dragEnterEvent(self, e):
-        if (e.mimeData().hasUrls()):
-            e.acceptProposedAction()
-
-    def _lineEdit_dropEvent(self, e):
-#        self.lineEdit.setText(e.mimeData().urls()[0].toLocalFile())
-        consolemanager.cexec(e.mimeData().urls()[0].toLocalFile())
 
