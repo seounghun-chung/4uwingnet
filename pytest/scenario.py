7b4f561df35d8fed3618b198b2864533090a4db2
seounghun.chung <4uwingnet@naver.com>
2019-07-06 14:33:01 +0900

    temporary save

--- pytest/scenario.py
+++ pytest/scenario.py
@@ -3,6 +3,7 @@ import bitstruct
 import copy
 import struct
 import configparser
+import re
 
 from canpacket import CanPacket
 from itertools import cycle
@@ -10,16 +11,26 @@ from functools import partial
 from collections import OrderedDict
 
 _hex = lambda x : '%02X' % (x)
-_debug = lambda x : ' '.join(map(_hex,x))
+_debug = lambda x : ' '.join(map(_hex,x)) if type(x) is str else x
 
+class Error(Exception):
+    pass
+    
+    
 class Scenario(object):
     def __init__(self, *argv, **kwargs):
-        pass
+        self.alivecycle = cycle([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14])
+        self.base_txpacket = dict()
 
+    def set_send_function(self,pf_send):
+        ''' register send message virtual function '''
+        self._send = pf_send
+        
     def init(self,path):
         self.cfg = configparser.ConfigParser()
         self.cfg.read(path)
-        
+
+        self.base_txpacket = copy.deepcopy(self.cfg._sections['tx_default'])
         for k,v in self.cfg._sections['rx'].items():
             self.cfg._sections['rx'][k] = int(v)
         for k,v in self.cfg._sections['tx'].items():
@@ -29,52 +40,38 @@ class Scenario(object):
         self.rx = CanPacket(**self.cfg._sections['rx'])
         self.tx = CanPacket(**self.cfg._sections['tx'])
 
+        ''' test sequence parsing '''
         sequence_str = self.cfg.get('pattern','sequence').replace(' ','')
-        alivecycle = cycle([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14])
-
-        for step in sequence_str:                    
-            if step == 'o': # make normal packet  (alive update, crc update)
-                for k, v in self.cfg._sections['tx_default'].items():
-                    self.cfg._sections['tx_default'][k] = int(v)
-                    if (k == 'alive'):
-                        self.cfg._sections['tx_default'][k] = next(alivecycle)            
-                msg = self.tx.gen(**self.cfg._sections['tx_default'])
-                msg = self.tx.update_crc()
-                l = partial(self._send, int(self.cfg.get('TestConfig','txid'),16), msg)                   
-                self.sequence.append(l)
+        parsed_infos = re.findall(r'([<>]?)([a-zA-Z])(\d+)(\s*)', sequence_str)    
+        if ''.join([''.join(info) for info in parsed_infos]) != sequence_str:
+            raise Error("bad format '{}'".format(sequence_str))
+                    
+        for parsed_info in parsed_infos:   
+            step = parsed_info[1]
+            size = int(parsed_info[2])
+            
+            if size == 0:
+                raise Error("bad format '{}'".format(fmt + byte_order))
+            
+            if   step == 'o': # make normal packet  (alive update, crc update)
+                for _ in range(size):
+                    l,msg = self._make_ok_function(self.base_txpacket)
+                    self.sequence.append(l)
 
             elif step == 'c': # make crc packet  (alive update, no crc update)
-                for k, v in self.cfg._sections['tx_default'].items():
-                    self.cfg._sections['tx_default'][k] = int(v)
-                    if (k == 'alive'):
-                        self.cfg._sections['tx_default'][k] = next(alivecycle)            
-                msg = self.tx.gen(**self.cfg._sections['tx_default'])
-                l = partial(self._send, int(self.cfg.get('TestConfig','txid'),16), msg)                      
-                self.sequence.append(l)
+                for _ in range(size):
+                    l,msg = self._make_crc_function(self.base_txpacket)
+                    self.sequence.append(l)
                  
             elif step == 'a': # make alive packet  (alive update twice, crc update)
-                for k, v in self.cfg._sections['tx_default'].items():
-                    self.cfg._sections['tx_default'][k] = int(v)
-                    if (k == 'alive'):
-                        self.cfg._sections['tx_default'][k] = next(alivecycle)            
-                        self.cfg._sections['tx_default'][k] = next(alivecycle)            
-                        
-                msg = self.tx.gen(**self.cfg._sections['tx_default'])
-                msg = self.tx.update_crc()
-                l = partial(self._send, int(self.cfg.get('TestConfig','txid'),16), msg)
-                self.sequence.append(l)
+                for _ in range(size):
+                    l,msg = self._make_alive_function(self.base_txpacket)
+                    self.sequence.append(l)
                 
             elif step == 'f': # make alive packet  (alive update and set 0xf, crc update)
-                for k, v in self.cfg._sections['tx_default'].items():
-                    self.cfg._sections['tx_default'][k] = int(v)
-                    if (k == 'alive'):
-                        self.cfg._sections['tx_default'][k] = next(alivecycle)     
-                        self.cfg._sections['tx_default'][k] = 0xf                
-                        
-                msg = self.tx.gen(**self.cfg._sections['tx_default'])
-                msg = self.tx.update_crc()
-                l = partial(self._send, int(self.cfg.get('TestConfig','txid'),16), msg)       
-                self.sequence.append(l)
+                for _ in range(size):
+                    l,msg = self._make_alive_f_function(self.base_txpacket)
+                    self.sequence.append(l)
    
             elif step == 'q':
                 l = lambda x: self._check_fault(x, self.cfg.getint('TestConfig','rx_sig_qual'))
@@ -86,7 +83,47 @@ class Scenario(object):
 
             else:
                 raise RuntimeError('%c is not defined in sequence' % step)
+                
             print(step, _debug(msg))               
+
+            
+    def _make_ok_function(self, packet):
+        packet['alive'] = next(self.alivecycle)
+        msg = self.tx.gen(**packet)
+        msg = self.tx.update_crc()
+
+        def __packet(packet):
+            packet['alive'] = next(self.alivecycle)
+            msg = self.tx.gen(**packet)
+            msg = self.tx.update_crc()
+                
+        l = partial(self._send, int(self.cfg.get('TestConfig','txid'),16), __packet)                   
+        return (l,msg)
+    
+    def _make_crc_function(self,packet):
+        packet['alive'] = next(self.alivecycle)
+        msg = self.tx.gen(**packet)
+        
+        l = partial(self._send, int(self.cfg.get('TestConfig','txid'),16), msg)                      
+        return (l,msg)
+        
+    def _make_alive_function(self,packet):
+        packet['alive'] = next(self.alivecycle)
+        packet['alive'] = next(self.alivecycle)
+        msg = self.tx.gen(**packet)
+        msg = self.tx.update_crc()       
+        
+        l = partial(self._send, int(self.cfg.get('TestConfig','txid'),16), msg)                      
+        return (l,msg)
+        
+    def _make_alive_f_function(self,packet):
+        packet['alive'] = next(self.alivecycle)
+        packet['alive'] = 0xf
+        msg = self.tx.gen(**packet)
+        msg = self.tx.update_crc()        
+        
+        l = partial(self._send, int(self.cfg.get('TestConfig','txid'),16), msg)                      
+        return (l,msg)
         
     def _check_fault(self, recvmsg, expected):
         ''' check to receive expected value '''
@@ -100,19 +137,25 @@ class Scenario(object):
     def _send(self, canid, message, dummy):
         ''' virtual function , shall be defined before executing init function. (because of partial function)
             dummy is compatible with recv feature...... it is not used'''
-        print(canid, _debug(message), dummy)
+        print('ERR : set_send_function', canid, _debug(message), dummy)
         return True
         
+    def update_base_txpacket(self, signame, value):
+        self.base_txpacket[signame] = value    
+        
     def dbg(self):  
-        for seq in self.sequence:
-            r = seq(bytes.fromhex('2234abcd1234abcd'))
-            print(r)
+        index = 0
+        it = iter(self.sequence)
+
+        for seq in it:
+            r = seq(bytes.fromhex('2234abcd1234ab%02d' % index))
+            index += 1
             
 if __name__ == "__main__":
     a = Scenario()
     
     def send(canid, msg, dummy):
         print('hello',msg,dummy)
-    a._send = send
+#    a._send = send
     a.init('scenario/test1.cfg')
     a.dbg()
